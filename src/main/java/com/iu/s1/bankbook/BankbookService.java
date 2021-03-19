0b0c0aeab1b12935ee05672d3830acb0396dca48
sunghyun94 <dog0094s@naver.com>
2021-03-19 17:23:59 +0900

    3/19(최종)

---
+++ src/main/java/com/iu/s1/bankbook/BankbookService.java
@@ -0,0 +1,22 @@
+package com.iu.s1.bankbook;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankbookService {
+	
+	@Autowired
+	private BankBookDAO bankBookDAO;
+	
+	public BankBookDTO getSelect(BankBookDTO bankBookDTO)throws Exception{
+		return bankBookDAO.getSelect(bankBookDTO);
+	}
+	
+	public List<BankBookDTO> getList()throws Exception{
+		return bankBookDAO.getList();
+	}
+
+}
